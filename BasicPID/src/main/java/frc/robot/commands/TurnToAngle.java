59dcf240e2975e81054a622c427c58ea882c614a
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2021-09-21 10:51:58 -0700

    Changed where PID values get changed

--- BasicPID/src/main/java/frc/robot/commands/TurnToAngle.java
+++ BasicPID/src/main/java/frc/robot/commands/TurnToAngle.java
@@ -23,38 +23,45 @@ public class TurnToAngle extends PIDCommand {
   public TurnToAngle(double targetAngleDegrees, Drivetrain drive) {
     super(
         // The controller that the command will use
-        // new PIDController(DriveConstants.kTurnP, DriveConstants.kTurnI, DriveConstants.kTurnD),
-
-        new PIDController(table.getEntry("P").getDouble(1), 
+        new PIDController(DriveConstants.kTurnP, 
                           DriveConstants.kTurnI, 
-                          table.getEntry("D").getDouble(0)),
+                          DriveConstants.kTurnD),
+
         // This should return the measurement
         drive::getHeading,
+
         // This should return the setpoint (can also be a constant)
-        table.getEntry("Heading Angle").getDouble(0),
-        // targetAngleDegrees,
+        targetAngleDegrees,
+
         // This uses the output
         output -> {
           // Use the output here
           drive.turn(output/10);
         },
+        // Use addRequirements() here to declare subsystem dependencies.
         drive);
-    // Use addRequirements() here to declare subsystem dependencies.
+    
     // Configure additional PID options by calling `getController` here.
     getController().enableContinuousInput(-180, 180);
     getController().setTolerance(DriveConstants.kTurnToleranceDeg,
                                 DriveConstants.kTurnRateToleranceDegPerS);
+  }
 
-    
-    // System.out.println("Setpoint " + targetAngleDegrees);
+  public void initialize() {
+    super.initialize();
+    // Override PID parameters from Shuffleboard
+    getController().setSetpoint(table.getEntry("Heading Angle").getDouble(0.0));
+    getController().setP(table.getEntry("P").getDouble(1.0));
+    getController().setD(table.getEntry("D").getDouble(0.0));
+
+    SmartDashboard.putNumber("Setpoint", getController().getSetpoint());
+    SmartDashboard.putNumber("Got P", getController().getP());
+    SmartDashboard.putNumber("Got D", getController().getD());
   }
 
   public void execute() {
     // TODO Auto-generated method stub
-    super.execute();
-    SmartDashboard.putNumber("Setpoint", getController().getSetpoint());
-    SmartDashboard.putNumber("Got P", getController().getP());
-    SmartDashboard.putNumber("Got D", getController().getD());
+    super.execute(); 
   }
 
   // Returns true when the command should end.
